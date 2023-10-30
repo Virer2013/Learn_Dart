55364bfc8009b661dc842f715c6ec19fca95940a
Virer2013 <virer2007@mail.ru>
2023-10-30 09:06:34 +0300

    first commit

---
+++ [2] Основы/Функция main и комментарии/main.dart
@@ -0,0 +1,19 @@
+void main() {
+  print('Hello world!');
+
+  // 10
+  // 5+5
+  // 'Dart'
+  // variable
+
+  /* Comment 1
+  Comment 2
+  Comment 3 */
+
+  /// I am a documentation comment
+  /// at your service
+
+  /**
+   * Me, too!
+   */
+}
