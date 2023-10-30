368a76475a3d88b32837a7b34e7d14b6a7d14e40
Virer2013 <virer2007@mail.ru>
2023-10-30 10:36:52 +0300

    first commit

---
+++ [5] Sets/Математические операции над множествами/main.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+void main() {
+  // Creating a Set
+  final Set<int> setOne = {};
+  final setTwo = <int>{};
+
+  final setThree = {1, 2, 3, 3};
+  print(setThree);
+
+  var setFour = {1, 'hello', true};
+
+  // Checking the contents
+  final fruits = {'apple', 'pineapple', 'watermelon'};
+  print(fruits.contains('apple'));
+  print(fruits.contains('pear'));
+
+  // Adding and Removing single elements
+  final newFruits = <String>{};
+  newFruits.add('orange');
+  newFruits.add('pear');
+  newFruits.add('orange');
+  print(newFruits);
+
+  newFruits.remove('pear');
+  print(newFruits);
+
+  // Looping over the elements
+  for (final fruit in fruits) {
+    print('I\'m like $fruit');
+  }
+
+  // Copying Sets
+  final freshFruits = fruits.toSet();
+  print(fruits);
+  freshFruits.remove('watermelon');
+  print(fruits);
+  print(freshFruits);
+
+  // Other operations
+  var newFruits2 = {'pear', ...freshFruits, if (true) ...fruits};
+  print(newFruits2);
+
+  final randomGenerator = Random();
+  final randomList = <int>[];
+
+  for (int i = 0; i < 10; i++) {
+    final randomInt = randomGenerator.nextInt(10) + 1;
+    randomList.add(randomInt);
+  }
+  print(randomList);
+
+  final uniqueValue = <int>{};
+  final duplicates = <int>{};
+
+  for (int number in randomList) {
+    if (uniqueValue.contains(number)) {
+      duplicates.add(number);
+    }
+    uniqueValue.add(number);
+  }
+
+  print(duplicates);
+
+  // Intersections
+  final setA = {1, 2, 4, 3, 5};
+  final setB = {3, 6, 7, 5};
+  // final intersections = setA.intersection(setB);
+  // print(intersections);
+
+  // Unions
+  // final union = setA.union(setB);
+  // print(union);
+
+  // Difference
+  final differenceA = setA.difference(setB);
+  print(differenceA);
+
+  final differenceB = setB.difference(setA);
+  print(differenceB);
+}
