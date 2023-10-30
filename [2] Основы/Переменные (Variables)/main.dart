a06f12ec402fa203d889c2001d44d0b59083d965
Virer2013 <virer2007@mail.ru>
2023-10-30 09:19:06 +0300

    first commit

---
+++ [2] Основы/Переменные (Variables)/main.dart
@@ -0,0 +1,25 @@
+late int tweets;
+void main() {
+  var number = 10;
+  print(number);
+  number = 15;
+  print(number);
+
+  int like;
+  like = 5;
+  print(like);
+
+  tweets = 10;
+  print(tweets);
+
+  int? comment;
+  print(comment);
+
+  // personAge
+  // numberOfPeople
+  // gradePointAverage
+
+  // a
+  // temp
+  // average
+}
